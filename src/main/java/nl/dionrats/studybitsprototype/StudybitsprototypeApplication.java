1a79be19c71b33f92842b13ab4cde89ec19310b6
Dion Rats <dionrats@Dions-MBP.mgmt.quintor.lan>
2019-05-09 13:55:24 +0200

    initial project

---
+++ src/main/java/nl/dionrats/studybitsprototype/StudybitsprototypeApplication.java
@@ -0,0 +1,13 @@
+package nl.dionrats.studybitsprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybitsprototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudybitsprototypeApplication.class, args);
+    }
+
+}

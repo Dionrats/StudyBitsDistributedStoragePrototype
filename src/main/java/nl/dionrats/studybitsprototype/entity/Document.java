39ea432f0d3cfd52f59c13061f1cff59d258ceff
Dionrats <dionrats@gmail.com>
2019-05-13 10:29:10 +0200

    implemented mongodb prototype

--- src/main/java/nl/dionrats/studybitsprototype/entity/Document.java
+++ src/main/java/nl/dionrats/studybitsprototype/entity/Document.java
@@ -2,9 +2,11 @@ package nl.dionrats.studybitsprototype.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Document {
     private String name;
     private String type;
---
+++ src/main/java/nl/dionrats/studybitsprototype/repository/MongoRepository.java
@@ -0,0 +1,39 @@
+package nl.dionrats.studybitsprototype.repository;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import lombok.extern.slf4j.Slf4j;
+import nl.dionrats.studybitsprototype.adapters.DocumentAdapter;
+import nl.dionrats.studybitsprototype.entity.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+@Slf4j
+@Repository("mongodb")
+public class MongoRepository implements IRepository {
+
+    private DBCollection collection;
+
+    @Autowired
+    public MongoRepository(MongoClient mongoClient) {
+        log.debug("======== Using Mongo Repository ======");
+        //TODO move dbname and collectionname to properties
+        this.collection = mongoClient.getDB("StudyBits").getCollection("Documents");
+    }
+
+    @Override
+    public Document getFile(String key) {
+        DBObject query = new BasicDBObject("_id", key);
+        return DocumentAdapter.toDocument(collection.find(query).one());
+    }
+
+    @Override
+    public void storeFile(String key, Document document) {
+        collection.insert(DocumentAdapter.toDBObject(key, document));
+    }
+
+
+}
